b4a8f6a694afd7a1b69a5fc544f95b75e3c2a053
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2024-02-12 18:07:40 +0900

    fix: :adhesive_bandage: 스타일 메니저. 글로벌 스타일 경로 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
-import './dtr/adaptor/managers/style/global.css'
+import './dtr/adaptor/managers/Style.manager.global.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App'
-import { QUERY_CLIENT_CONFIG } from './dtr/adaptor/managers/Network.manager'
+import NetworkManager from './dtr/adaptor/managers/Network.manager'
 
-const queryClient = new QueryClient(QUERY_CLIENT_CONFIG)
+const queryClient = new QueryClient(NetworkManager.QUERY_CLIENT_CONFIG)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
